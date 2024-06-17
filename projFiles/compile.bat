501d061af957acd52ea7b29aaaab3c0a5d4c95dd
DuskieWhy <121409172+DuskieWhy@users.noreply.github.com>
2024-06-16 23:55:45 -0400

    Update compile.bat

--- projFiles/compile.bat
+++ projFiles/compile.bat
@@ -9,7 +9,7 @@ haxelib set flixel 5.2.2
 haxelib set lime 8.1.1
 haxelib set openfl 9.2.2
 haxelib set hscript 2.5.0
-haxelib set hxvlc
+haxelib set hxvlc 1.2.0
 haxelib set hxcpp 4.3.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
