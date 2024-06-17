78c45a62f8f7f16df8b949265191be3064c5798b
DuskieWhy <121409172+DuskieWhy@users.noreply.github.com>
2024-06-16 23:53:42 -0400

    Update compile.bat

--- projFiles/compile.bat
+++ projFiles/compile.bat
@@ -8,7 +8,7 @@ haxelib set flixel-ui 2.5.0
 haxelib set flixel 5.2.2
 haxelib set lime 8.1.1
 haxelib set openfl 9.2.2
-haxelib install hscript
+haxelib set hscript 2.5.0
 haxelib set hxvlc
 haxelib set hxcpp 4.3.2
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
