8450f5abdf772188012c80cf2f6af6c32b5cf7f4
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-09-19 15:43:08 -0400

    that's a windows thing, on linux it just gives a syntax error, windows is weird.

--- projFiles/compile-unix.sh
+++ projFiles/compile-unix.sh
@@ -16,5 +16,4 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
 echo BUILDING GAME
 lime test cpp
-echo.
 echo done.
