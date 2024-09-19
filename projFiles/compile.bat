8b257247345403cde60a7c423fad321b31684522
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-09-19 15:44:05 -0400

    flxanimate windows

--- projFiles/compile.bat
+++ projFiles/compile.bat
@@ -11,6 +11,7 @@ haxelib set openfl 9.2.2
 haxelib set hscript 2.5.0
 haxelib set hxvlc 1.2.0
 haxelib set hxcpp 4.3.2
+haxelib set flxanimate 3.0.4
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
 echo BUILDING GAME
