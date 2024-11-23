a069388dde35dcc38febc975f176d9ac78f0480e
crowplexus <45212377+crowplexus@users.noreply.github.com>
2024-11-23 12:12:10 -0400

    fix weird formatting here

--- source/funkin/data/Rating.hx
+++ source/funkin/data/Rating.hx
@@ -6,16 +6,15 @@ class Rating
 {
 	public static function judgeNote(note:Note, diff:Float = 0):Rating // die
 	{
-        var data = PlayState.instance.ratingsData;
+		var data = PlayState.instance.ratingsData;
 		switch (note.noteType) { // custom notetypes get custom ways of being judged
-			//case "mine": // example case, replace if needed
+			// case "mine": // example case, replace if needed
 			default:
 				// hey NVE devs, i think you could call noteScript.executeFunc("onJudgeNote", [diff]) here
 				// just in case other notetypes need to change how they are judged
 				// up to you, though @crowplexus
-				for(i in 0...data.length-1)
-					if (diff <= data[i].hitWindow)
-						return data[i];
+				for (i in 0...data.length - 1)
+					if (diff <= data[i].hitWindow) return data[i];
 		}
 		return data[data.length - 1];
 	}
@@ -27,17 +26,16 @@ class Rating
 	**/
 	public static function judgeTime(time:Float = 0):Rating
 	{
-        var data = PlayState.instance.ratingsData;
-		for(i in 0...data.length-1)
-			if (time <= data[i].hitWindow)
-				return data[i];
+		var data = PlayState.instance.ratingsData;
+		for (i in 0...data.length - 1)
+			if (time <= data[i].hitWindow) return data[i];
 		return data[data.length - 1];
 	}
 
 	public var name:String = '';
 	public var image:String = '';
 	public var counter:String = '';
-	public var hitWindow:Null<Float> = 0; //ms
+	public var hitWindow:Null<Float> = 0; // ms
 	public var ratingMod:Float = 1;
 	public var score:Int = 350;
 	public var noteSplash:Bool = true;
@@ -48,8 +46,7 @@ class Rating
 		this.image = name;
 		this.counter = name + 's';
 		this.hitWindow = Reflect.field(ClientPrefs, name + 'Window');
-		if(hitWindow == null)
-			hitWindow = 0;
+		if (hitWindow == null) hitWindow = 0;
 	}
 
 	public function increase(blah:Int = 1)
