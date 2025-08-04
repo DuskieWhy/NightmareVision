435abc16e30e00ed4a88f71ca53c1e17be80f561
data5 <99661153+FixedData@users.noreply.github.com>
2025-08-04 13:37:55 -0600

    formatter

--- content/base-game/stages/mallEvil.hx
+++ content/base-game/stages/mallEvil.hx
@@ -1,49 +1,52 @@
-function onLoad(){
-    var bg:BGSprite = new BGSprite('christmas/evilBG', -400, -500, 0.2, 0.2);
-    bg.setGraphicSize(Std.int(bg.width * 0.8));
-    bg.updateHitbox();
-    add(bg);
-
-    var evilTree:BGSprite = new BGSprite('christmas/evilTree', 300, -300, 0.2, 0.2);
-    add(evilTree);
-
-    var evilSnow:BGSprite = new BGSprite('christmas/evilSnow', -200, 700);
-    add(evilSnow);
+function onLoad()
+{
+	var bg:BGSprite = new BGSprite('christmas/evilBG', -400, -500, 0.2, 0.2);
+	bg.setGraphicSize(Std.int(bg.width * 0.8));
+	bg.updateHitbox();
+	add(bg);
+	
+	var evilTree:BGSprite = new BGSprite('christmas/evilTree', 300, -300, 0.2, 0.2);
+	add(evilTree);
+	
+	var evilSnow:BGSprite = new BGSprite('christmas/evilSnow', -200, 700);
+	add(evilSnow);
 }
 
 var allowCountdown:Bool = false;
-function onStartCountdown(){
-    if(!allowCountdown){
-        var blackScreen:FlxSprite = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(FlxG.height * 2), FlxColor.BLACK);
-        add(blackScreen);
-        blackScreen.scrollFactor.set();
-        camHUD.visible = false;
-        inCutscene = true;
 
-        FlxTween.tween(blackScreen, {alpha: 0}, 0.7, {
-            ease: FlxEase.linear,
-            onComplete: function(twn:FlxTween) {
-                remove(blackScreen);
-            }
-        });
-        FlxG.sound.play(Paths.sound('Lights_Turn_On'));
-        snapCamToPos(400, -2050);
-        FlxG.camera.zoom = 1.5;
-
-        new FlxTimer().start(0.8, function(tmr:FlxTimer)
-        {
-            camHUD.visible = true;
-            FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 2.5, {
-                ease: FlxEase.quadInOut,
-                onComplete: function(twn:FlxTween)
-                {
-                    allowCountdown = true;
-                    inCutscene = false;
-                    startCountdown();
-                }
-            });
-        });
-        return Function_Stop;
-    }
-
+function onStartCountdown()
+{
+	if (!allowCountdown)
+	{
+		var blackScreen:FlxSprite = new FlxSprite().makeGraphic(Std.int(FlxG.width * 2), Std.int(FlxG.height * 2), FlxColor.BLACK);
+		add(blackScreen);
+		blackScreen.scrollFactor.set();
+		camHUD.visible = false;
+		inCutscene = true;
+		
+		FlxTween.tween(blackScreen, {alpha: 0}, 0.7,
+			{
+				ease: FlxEase.linear,
+				onComplete: function(twn:FlxTween) {
+					remove(blackScreen);
+				}
+			});
+		FlxG.sound.play(Paths.sound('Lights_Turn_On'));
+		snapCamToPos(400, -2050);
+		FlxG.camera.zoom = 1.5;
+		
+		new FlxTimer().start(0.8, function(tmr:FlxTimer) {
+			camHUD.visible = true;
+			FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 2.5,
+				{
+					ease: FlxEase.quadInOut,
+					onComplete: function(twn:FlxTween) {
+						allowCountdown = true;
+						inCutscene = false;
+						startCountdown();
+					}
+				});
+		});
+		return Function_Stop;
+	}
 }
--- content/base-game/stages/stage.hx
+++ content/base-game/stages/stage.hx
@@ -1,33 +1,36 @@
 import funkin.data.Conductor;
 
-function onLoad() 
+function onLoad()
 {
-    var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image("stageback"));
-	add(bg); 
-
-    var stageFront:FlxSprite = new FlxSprite(-600, 600).loadGraphic(Paths.image("stagefront"));
-    add(stageFront);
-
-    var stageCurtains:FlxSprite = new FlxSprite(-600, -300).loadGraphic(Paths.image("stagecurtains"));
-    add(stageCurtains);
-    stageCurtains.zIndex = 9999;
+	var bg:FlxSprite = new FlxSprite(-600, -200).loadGraphic(Paths.image("stageback"));
+	add(bg);
+	
+	var stageFront:FlxSprite = new FlxSprite(-600, 600).loadGraphic(Paths.image("stagefront"));
+	add(stageFront);
+	
+	var stageCurtains:FlxSprite = new FlxSprite(-600, -300).loadGraphic(Paths.image("stagecurtains"));
+	add(stageCurtains);
+	stageCurtains.zIndex = 9999;
 }
 
-function onUpdate(elapsed){
-    if(dad.curCharacter == 'gf') camZooming = false;
+function onUpdate(elapsed)
+{
+	if (dad.curCharacter == 'gf') camZooming = false;
 }
 
 var t = false;
 var t2 = 'boyfriend';
-function onMoveCamera(turn){
-    if(PlayState.SONG.song.toLowerCase() != 'tutorial') return;
 
-    if(t2 != turn) t = true;
-    t2 = turn;
-
-    if(t){
-        FlxTween.tween(FlxG.camera, {zoom: turn == 'dad' ? 1.3 : 1}, (Conductor.stepCrotchet / 1000) * 4, {ease: FlxEase.elasticInOut});
-        t = false;
-    }
-
+function onMoveCamera(turn)
+{
+	if (PlayState.SONG.song.toLowerCase() != 'tutorial') return;
+	
+	if (t2 != turn) t = true;
+	t2 = turn;
+	
+	if (t)
+	{
+		FlxTween.tween(FlxG.camera, {zoom: turn == 'dad' ? 1.3 : 1}, (Conductor.stepCrotchet / 1000) * 4, {ease: FlxEase.elasticInOut});
+		t = false;
+	}
 }
