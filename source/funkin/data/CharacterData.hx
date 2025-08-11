9e7e3c2977664c5b22eb80ad67c49e87a348aab9
data5 <99661153+FixedData@users.noreply.github.com>
2025-08-11 16:54:36 -0600

    rewrote the parser

--- source/funkin/data/CharacterData.hx
+++ source/funkin/data/CharacterData.hx
@@ -6,9 +6,14 @@ import haxe.xml.Access;
 
 import funkin.objects.Character;
 
+@:nullSafety
 class CharacterParser
 {
-	// wipppppp
+	/**
+	 * Fetches a characters data structure to be loaded to a character
+	 * 
+	 * supported formats are `psych`, `cne`, and `vslice`
+	 */
 	public static function fetchInfo(id:String):CharacterInfo
 	{
 		var charPath = Paths.findFileAndAddExts('characters/$id', ['json', 'xml']);
@@ -18,14 +23,14 @@ class CharacterParser
 			charPath = Paths.getPrimaryPath('characters/${Character.DEFAULT_CHARACTER}.json');
 		}
 		
-		var raw:Null<String> = null;
+		var raw:String = '';
 		
 		try
 		{
 			raw = FunkinAssets.getContent(charPath);
 		}
 		
-		if (charPath.endsWith('.xml')) return fromCNE(raw); // check if its cne
+		if (raw.length != 0 && charPath.endsWith('.xml')) return fromCNE(raw); // if it was a xml its cne
 		
 		final rawJson:Null<Any> = FunkinAssets.parseJson(raw);
 		
@@ -40,205 +45,340 @@ class CharacterParser
 		return cast rawJson;
 	}
 	
-	// todo clean this it makes me sad to look at
-	// dont expect this to be that good aha wip
-	public static function fromCNE(data:Dynamic):CharacterInfo
+	/**
+	 * Converts a cne character xml into a usable format for the engine
+	 * 
+	 * This does not parse every possible cne attribute so some things may be off
+	 */
+	public static function fromCNE(data:String):CharacterInfo
 	{
-		var xml = Xml.parse(data).firstElement();
+		final xml = Xml.parse(data).firstElement();
 		
 		final access = new Access(xml);
 		
-		final animations:Array<AnimationInfo> = [];
+		final baseInfo:CharacterInfo = getTemplateCharInfo();
 		
 		// not used for this sorry ig.
-		final isPlayer = access.x.exists('isPlayer') ? access.x.get('isPlayer') == 'true' : false;
+		// final isPlayer = access.x.exists('isPlayer') ? access.x.get('isPlayer') == 'true' : false;
 		
-		// from https://github.com/CodenameCrew/CodenameEngine/blob/main/source/funkin/backend/utils/CoolUtil.hx
-		inline function parseNumberRange(input:String):Array<Int>
+		if (access.has.sprite) baseInfo.image = 'characters/' + access.att.sprite;
+		
+		if (access.has.holdTime)
 		{
-			var result:Array<Int> = [];
-			var parts:Array<String> = input.split(",");
-			
-			for (part in parts)
-			{
-				part = part.trim();
-				var idx = part.indexOf("..");
-				if (idx != -1)
-				{
-					var start = Std.parseInt(part.substring(0, idx).trim());
-					var end = Std.parseInt(part.substring(idx + 2).trim());
-					
-					if (start == null || end == null)
-					{
-						continue;
-					}
-					
-					if (start < end)
-					{
-						for (j in start...end + 1)
-						{
-							result.push(j);
-						}
-					}
-					else
-					{
-						for (j in end...start + 1)
-						{
-							result.push(start + end - j);
-						}
-					}
-				}
-				else
-				{
-					var num = Std.parseInt(part);
-					if (num != null)
-					{
-						result.push(num);
-					}
-				}
-			}
-			return result;
+			final singDuration = Std.parseFloat(access.att.holdTime);
+			if (!Math.isNaN(singDuration)) baseInfo.sing_duration = singDuration;
 		}
+		
+		if (access.has.antialiasing) baseInfo.no_antialiasing = access.att.antialiasing == 'false';
+		
+		if (access.has.scale)
+		{
+			final scale = Std.parseFloat(access.att.scale);
+			if (!Math.isNaN(scale)) baseInfo.scale = scale;
+		}
+		
+		if (access.has.x)
+		{
+			final x = Std.parseFloat(access.att.x);
+			if (!Math.isNaN(x)) baseInfo.position[0] = x;
+		}
+		
+		if (access.has.y)
+		{
+			final y = Std.parseFloat(access.att.y);
+			if (!Math.isNaN(y)) baseInfo.position[1] = y;
+		}
+		
+		if (access.has.icon) baseInfo.healthicon = access.att.icon;
+		
+		if (access.has.flipX) baseInfo.flip_x = access.att.flipX == 'true';
+		
+		if (access.has.interval)
+		{
+			final danceEvery = Std.parseInt(access.att.interval);
+			if (danceEvery != null) baseInfo.dance_every = danceEvery;
+		}
+		
+		if (access.has.camx)
+		{
+			final x = Std.parseFloat(access.att.camx);
+			if (!Math.isNaN(x)) baseInfo.camera_position[0] = x;
+		}
+		
+		if (access.has.camy)
+		{
+			final y = Std.parseFloat(access.att.camy);
+			if (!Math.isNaN(y)) baseInfo.camera_position[1] = y;
+		}
+		
+		if (access.has.color)
+		{
+			final colour = FlxColor.fromString(access.att.color);
+			if (colour != null) baseInfo.healthbar_colour = colour;
+		}
+		
+		if (access.has.gameOverChar) baseInfo.gameover_character = access.att.gameOverChar;
 		
 		for (node in access.elements)
 		{
 			if (node.name == 'anim')
 			{
-				final anim = node.x.get('name');
-				final prefix = node.x.get('anim');
-				final xOffset = node.x.exists('x') ? Std.parseInt(node.x.get('x')) : 0;
-				final yOffset = node.x.exists('y') ? Std.parseInt(node.x.get('y')) : 0;
-				final fps = node.x.exists('fps') ? Std.parseInt(node.x.get('fps')) : 24;
-				final loops = node.x.exists('loop') ? node.x.get('loop') == 'true' : false;
-				final indices = node.x.exists('indices') ? parseNumberRange(node.x.get('indices')) : [];
-				
-				animations.push(
-					{
-						anim: anim,
-						name: prefix,
-						offsets: [xOffset, yOffset],
-						fps: fps,
-						loop: loops,
-						indices: indices
-					});
+				baseInfo.animations.push(cneAnimationToPsych(node));
 			}
 		}
 		
-		final texture = 'characters/' + (access.x.exists('sprite') ? access.x.get('sprite') : 'BOYFRIEND');
-		final flipX = access.x.exists('flipX') ? access.x.get('flipX') == 'true' : false;
-		final icon = access.x.exists('icon') ? access.x.get('icon') : 'face';
-		final healthColour = access.x.exists('color') ? FlxColor.fromString(access.x.get('color')) : FlxColor.GRAY;
-		final x = access.x.exists('x') ? Std.parseFloat(access.x.get('x')) : 0.0;
-		final y = access.x.exists('y') ? Std.parseFloat(access.x.get('y')) : 0.0;
-		final antialiasing = access.x.exists('antialiasing') ? true : true;
-		final scale = access.x.exists('scale') ? Std.parseFloat(access.x.get('scale')) : 1;
-		final singDuration = access.x.exists('holdTime') ? Std.parseFloat(access.x.get('holdTime')) : 6.1;
-		final danceEvery = access.x.exists('interval') ? Std.parseInt(access.x.get('interval')) : 2;
-		
-		final info:CharacterInfo =
-			{
-				image: texture,
-				position: [x, y],
-				camera_position: [0, 0],
-				animations: animations,
-				healthbar_colour: healthColour,
-				healthicon: icon,
-				flip_x: flipX,
-				sing_duration: singDuration,
-				scale: scale,
-				no_antialiasing: !antialiasing,
-				dance_every: danceEvery
-			}
-			
-		return info;
+		return baseInfo;
 	}
 	
-	// wip
+	/**
+	 * parses a VSlice character json into a usable format for the engine
+	 */
 	public static function fromVSlice(data:Dynamic):CharacterInfo
 	{
-		final singDuration = data?.singTime ?? 6.1;
-		final antialiasing = !data?.isPixel ?? true;
-		final scale = data?.scale ?? 1.0;
-		final danceEvery = data?.danceEvery ?? 2;
-		final flipX = data?.flipX ?? false;
-		final cameraPosition:Array<Float> = cast data?.cameraOffsets ?? [0, 0];
-		final position:Array<Float> = cast data?.offsets ?? [0, 0];
-		final icon = data?.healthIcon?.id ?? 'face';
+		final baseInfo:CharacterInfo = getTemplateCharInfo();
 		
-		final animations:Array<AnimationInfo> = [];
+		if (data == null) return baseInfo;
+		
+		final data:VSliceCharacterInfo = data;
 		
 		final assetPaths:Array<String> = [];
-		if (data?.assetPath != null) assetPaths.push(data.assetPath);
 		
-		for (i in 0...data.animations.length)
+		if (data.assetPath != null) assetPaths.push(data.assetPath);
+		
+		if (data.singTime != null) baseInfo.sing_duration = data.singTime;
+		
+		if (data.isPixel != null) baseInfo.no_antialiasing = data.isPixel;
+		
+		if (data.scale != null) baseInfo.scale = data.scale;
+		
+		if (data.offsets != null) baseInfo.position = data.offsets;
+		
+		if (data.healthIcon != null && data.healthIcon.id != null) baseInfo.healthicon = data.healthIcon.id;
+		
+		if (data.flipX != null) baseInfo.flip_x = data.flipX;
+		
+		if (data.danceEvery != null)
 		{
-			//
-			final vSliceAnim = data.animations[i];
-			if (vSliceAnim != null)
+			final danceEvery = Std.int(data.danceEvery);
+			if (!Math.isNaN(danceEvery)) baseInfo.dance_every = danceEvery;
+		}
+		
+		if (data.cameraOffsets != null) baseInfo.camera_position = data.cameraOffsets;
+		
+		if (data.animations != null)
+		{
+			for (anim in data.animations)
 			{
-				// maybe i could make a cleaner converter with some extra funcs and typedefs...
-				final flipX = vSliceAnim?.flipX ?? false;
-				final flipY = vSliceAnim?.flipY ?? false;
-				final offsets = vSliceAnim?.offsets ?? [0, 0];
-				final fps = vSliceAnim?.frameRate ?? 24;
-				final prefix = vSliceAnim?.prefix ?? '';
-				final animTag = vSliceAnim?.name ?? '';
-				final indices = vSliceAnim?.frameIndices ?? [];
-				final looped = vSliceAnim?.looped ?? false;
-				
-				if (vSliceAnim.assetPath != null)
-				{
-					if (!assetPaths.contains(vSliceAnim.assetPath)) assetPaths.push(vSliceAnim.assetPath); // for multisparrow stuff
-				}
-				
-				final animToPush:AnimationInfo =
-					{
-						flipY: flipY,
-						flipX: flipX,
-						offsets: offsets,
-						fps: fps,
-						anim: animTag,
-						name: prefix,
-						loop: looped,
-						indices: indices
-					}
-					
-				animations.push(animToPush);
+				baseInfo.animations.push(vSliceAnimationToPsych(anim));
+				if (anim.assetPath != null && !assetPaths.contains(anim.assetPath)) assetPaths.push(anim.assetPath);
 			}
 		}
 		
-		final info:CharacterInfo =
+		baseInfo.image = assetPaths.join(',');
+		
+		return baseInfo;
+	}
+	
+	/**
+	 * converts a VSlice animation into a `AnimationInfo`
+	 */
+	static function vSliceAnimationToPsych(anim:VSliceAnimationInfo):AnimationInfo
+	{
+		final baseInfo:AnimationInfo = getTemplateAnimInfo();
+		
+		if (anim == null) return baseInfo;
+		
+		if (anim.offsets != null) baseInfo.offsets = [for (i in anim.offsets) Std.int(i)];
+		
+		if (anim.flipX != null) baseInfo.flipX = anim.flipX;
+		
+		if (anim.flipY != null) baseInfo.flipY = anim.flipY;
+		
+		if (anim.frameRate != null) baseInfo.fps = anim.frameRate;
+		
+		if (anim.frameIndices != null) baseInfo.indices = anim.frameIndices;
+		
+		if (anim.looped != null) baseInfo.loop = anim.looped;
+		
+		if (anim.name != null) baseInfo.anim = anim.name;
+		
+		if (baseInfo.anim.endsWith('-hold')) baseInfo.anim = baseInfo.anim.replace('-hold', '-loop'); // base game calls em hold
+		
+		if (anim.prefix != null) baseInfo.name = anim.prefix;
+		
+		return baseInfo;
+	}
+	
+	/**
+	 * Parses a xml node from a cne character to a `AnimationInfo`
+	 */
+	static function cneAnimationToPsych(node:Access)
+	{
+		final baseInfo:AnimationInfo = getTemplateAnimInfo();
+		
+		if (node.name != 'anim') return baseInfo;
+		
+		if (node.has.name) baseInfo.anim = node.att.name;
+		
+		if (node.has.anim) baseInfo.name = node.att.anim;
+		
+		if (node.has.x)
+		{
+			final x = Std.parseInt(node.att.x);
+			if (x != null) baseInfo.offsets[0] = x;
+		}
+		
+		if (node.has.y)
+		{
+			final y = Std.parseInt(node.att.y);
+			if (y != null) baseInfo.offsets[1] = y;
+		}
+		
+		if (node.has.fps)
+		{
+			final fps = Std.parseInt(node.att.fps);
+			if (fps != null) baseInfo.fps = fps;
+		}
+		
+		if (node.has.indices) baseInfo.indices = parseNumberRange(node.att.fps);
+		
+		return baseInfo;
+	}
+	
+	/**
+	 * Needed for cne anim parsing
+	 * 
+	 * https://github.com/CodenameCrew/CodenameEngine/blob/main/source/funkin/backend/utils/CoolUtil.hx
+	 */
+	inline static function parseNumberRange(input:String):Array<Int>
+	{
+		var result:Array<Int> = [];
+		var parts:Array<String> = input.split(",");
+		
+		for (part in parts)
+		{
+			part = part.trim();
+			var idx = part.indexOf("..");
+			if (idx != -1)
 			{
-				scale: scale,
-				flip_x: flipX,
-				camera_position: cameraPosition,
-				position: position,
-				no_antialiasing: !antialiasing,
-				sing_duration: singDuration,
-				healthicon: icon,
-				healthbar_colour: FlxColor.GRAY,
-				image: assetPaths.join(','),
-				animations: animations,
-				dance_every: danceEvery
-			};
-			
-		return info;
+				var start = Std.parseInt(part.substring(0, idx).trim());
+				var end = Std.parseInt(part.substring(idx + 2).trim());
+				
+				if (start == null || end == null)
+				{
+					continue;
+				}
+				
+				if (start < end)
+				{
+					for (j in start...end + 1)
+					{
+						result.push(j);
+					}
+				}
+				else
+				{
+					for (j in end...start + 1)
+					{
+						result.push(start + end - j);
+					}
+				}
+			}
+			else
+			{
+				var num = Std.parseInt(part);
+				if (num != null)
+				{
+					result.push(num);
+				}
+			}
+		}
+		return result;
+	}
+	
+	static function getTemplateCharInfo():CharacterInfo
+	{
+		return {
+			sing_duration: 4,
+			no_antialiasing: false,
+			flip_x: false,
+			healthicon: 'face',
+			healthbar_colour: FlxColor.GRAY,
+			image: 'characters/BOYFRIEND',
+			dance_every: 2,
+			position: [0, 0],
+			camera_position: [0, 0],
+			animations: [],
+			scale: 1
+		};
+	}
+	
+	static function getTemplateAnimInfo():AnimationInfo
+	{
+		return {
+			offsets: [0, 0],
+			flipX: false,
+			flipY: false,
+			fps: 24,
+			indices: [],
+			loop: false,
+			anim: '',
+			name: ''
+		};
 	}
 }
 
+/**
+ * The animation structure to be used when loading a character's animations
+ */
 typedef AnimationInfo =
 {
+	/**
+	 * The animation name/label
+	 */
 	var anim:String;
+	
+	/**
+	 * The prefix or symbol name for a animation
+	 */
 	var name:String;
+	
+	/**
+	 * The framerate for this animation
+	 * 
+	 * - Default is 24
+	 */
 	var fps:Int;
+	
+	/**
+	 * Whether the animation should loop
+	 */
 	var loop:Bool;
+	
+	/**
+	 * The given frame order for the animation
+	 */
 	var indices:Array<Int>;
+	
+	/**
+	 * The offsets to the animation
+	 */
 	var offsets:Array<Int>;
 	
+	/**
+	 * Whether this animation should be flipped horizontally
+	 */
 	var ?flipX:Bool;
+	
+	/**
+	 * Whether this animation should be flipped vertically
+	 */
 	var ?flipY:Bool;
 }
 
+/**
+ * The structure used to load a character
+ */
 typedef CharacterInfo =
 {
 	/**
@@ -321,4 +461,68 @@ typedef CharacterInfo =
 	var ?gameover_loop_sound:String;
 	
 	var ?gameover_confirm_sound:String;
+}
+
+/**
+ * Simplified structure of a VSlice characters data
+ * 
+ * used for conversion
+ */
+private typedef VSliceCharacterInfo =
+{
+	var ?singTime:Float;
+	
+	var ?isPixel:Bool;
+	
+	var ?danceEvery:Float;
+	
+	var ?scale:Float;
+	
+	var ?flipX:Bool;
+	
+	var ?cameraOffsets:Array<Float>;
+	
+	var ?offsets:Array<Float>;
+	
+	var ?assetPath:String;
+	
+	var ?healthIcon:VSliceIconInfo;
+	
+	var ?animations:Array<VSliceAnimationInfo>;
+}
+
+/**
+ * Simplified structure of a VSlice characters health icon data
+ * 
+ * used for conversion
+ */
+private typedef VSliceIconInfo =
+{
+	var ?id:String;
+}
+
+/**
+ * Simplified structure of a VSlice characters animation data
+ * 
+ * used for conversion
+ */
+private typedef VSliceAnimationInfo =
+{
+	var ?flipX:Bool;
+	
+	var ?flipY:Bool;
+	
+	var ?offsets:Array<Float>;
+	
+	var ?frameRate:Int;
+	
+	var ?frameIndices:Array<Int>;
+	
+	var ?assetPath:String;
+	
+	var ?looped:Bool;
+	
+	var ?name:String;
+	
+	var ?prefix:String;
 }
