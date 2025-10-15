e6741a13defff4b05c7a4429af05381a44aac013
data5 <99661153+FixedData@users.noreply.github.com>
2025-10-15 15:16:16 -0600

    these should be declared finals

--- source/funkin/scripting/ScriptConstants.hx
+++ source/funkin/scripting/ScriptConstants.hx
@@ -13,19 +13,19 @@ class ScriptConstants
 	/**
 	 * If returned in a script function, it's normal behavior will stop
 	 */
-	public static var Function_Stop:Dynamic = 1;
+	public static final Function_Stop:Dynamic = 1;
 	
 	/**
 	 * If returned in a script function, it's normal behavior will continue
 	 * 
 	 * This is the regular return in a `ScriptGroup`
 	 */
-	public static var Function_Continue:Dynamic = 0;
+	public static final Function_Continue:Dynamic = 0;
 	
 	/**
 	 * Used in `ScriptGroup`, if Returned with in the group, the function will not be called on any remaining scripts that have yet to recieve this function call.
 	 */
-	public static var Function_Halt:Dynamic = 2;
+	public static final Function_Halt:Dynamic = 2;
 	
 	/**
 	 * Gets the current state
