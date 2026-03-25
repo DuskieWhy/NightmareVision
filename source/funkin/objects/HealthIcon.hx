8d3454a748dbfb10e99690791c0fe51b66893267
Ender69 <112335251+ender69flock@users.noreply.github.com>
2026-03-26 02:25:20 +0500

    iSize and animation

--- source/funkin/objects/HealthIcon.hx
+++ source/funkin/objects/HealthIcon.hx
@@ -39,9 +39,15 @@ class HealthIcon extends FlxSprite implements IUiSprite
 	/** 
 	 * Used for dividing icon based on how many frames it has
 	**/
-	public var frameCount(default, set):Int = 2;
+	public var frameCount(default, set):Int = 3;
 	
 	public var alphaMultipler(default, set):Float = 1;
+	
+	// Used to determine if the icon is animated or not, if true it'll try to load an xml file with the same name as the image
+	public var isAnimated:Bool = false;
+	
+	// Used to determine if the icon has a winning or not
+	public var hasWinIcon:Bool = false;
 	
 	function set_alphaMultipler(v:Float):Float
 	{
@@ -61,7 +67,6 @@ class HealthIcon extends FlxSprite implements IUiSprite
 	{
 		frameCount = value;
 		changeIcon(characterName, true);
-		
 		return value;
 	}
 	
@@ -97,20 +102,37 @@ class HealthIcon extends FlxSprite implements IUiSprite
 		if (!Paths.fileExists('images/' + name + '.png')) name = 'icons/icon-' + char; // Older versions of psych engine's support
 		if (!Paths.fileExists('images/' + name + '.png')) name = 'icons/icon-face'; // Prevents crash from missing icon
 		
-		final graphic = Paths.image(name, null, false);
-		var iSize:Float = Math.round(graphic.width / graphic.height);
-		loadGraphic(graphic, true, Math.floor(graphic.width / frameCount), Math.floor(graphic.height));
-		iconOffsets[0] = (width - 150) / 2;
-		iconOffsets[1] = (width - 150) / 2;
-		updateHitbox();
+		var animToFind:String = Paths.getPath(name + '.xml', null, false);
 		
-		var c = [];
-		for (i in 0...frameCount)
-			c.push(i);
+		if (FunkinAssets.exists(animToFind))
+		{
+			isAnimated = true;
 			
-		animation.add(char, c, 0, false, isPlayer);
-		animation.play(char); // i do plan on adding more functionality to icons at a later date
-		
+			final graphic = Paths.getSparrowAtlas(name, null, false);
+			frames = graphic;
+			
+			animation.addByPrefix('idle', 'idle', 24, true, isPlayer);
+			animation.addByPrefix('winning', 'winning', 24, true, isPlayer);
+			animation.addByPrefix('losing', 'losing', 24, true, isPlayer);
+			animation.addByPrefix('toWinning', 'toWinning', 24, false, isPlayer);
+			animation.addByPrefix('toLosing', 'toLosing', 24, false, isPlayer);
+			animation.addByPrefix('fromWinning', 'fromWinning', 24, false, isPlayer);
+			animation.addByPrefix('fromLosing', 'fromLosing', 24, false, isPlayer);
+			animation.play('idle');
+			iconOffsets[0] = (width - 150);
+			iconOffsets[1] = (height - 150);
+		}
+		else
+		{
+			final graphic = Paths.image(name, null, false);
+			var iSize:Float = Math.round(graphic.width / graphic.height);
+			loadGraphic(graphic, true, Math.floor(graphic.width / iSize), Math.floor(graphic.height));
+			iconOffsets[0] = (width - 150) / iSize;
+			iconOffsets[1] = (height - 150) / iSize;
+			animation.add(char, [for (i in 0...frames.frames.length) i], 0, false, isPlayer);
+			animation.play(char); // i do plan on adding more functionality to icons at a later date
+		}
+		updateHitbox();
 		antialiasing = char.endsWith('-pixel') ? false : ClientPrefs.globalAntialiasing;
 	}
 	
@@ -118,7 +140,8 @@ class HealthIcon extends FlxSprite implements IUiSprite
 	{
 		super.updateHitbox();
 		
-		if (updateOffset) {
+		if (updateOffset)
+		{
 			offset.x = iconOffsets[0];
 			offset.y = iconOffsets[1];
 		}
@@ -130,6 +153,20 @@ class HealthIcon extends FlxSprite implements IUiSprite
 		super.destroy();
 	}
 	
+	public function getCurrentAnimation():String
+	{
+		if (this.animation == null || this.animation.curAnim == null) return "";
+		return this.animation.curAnim.name;
+	}
+	
+	public function hasAnimation(id:String):Bool
+	{
+		if (animation == null) return false;
+		return animation.getByName(id) != null;
+	}
+	
+	public function isAnimationFinished():Bool return this.animation.finished;
+	
 	/**
 	 * Updates the current animation based on a value from 0 - 1.
 	 */
@@ -137,6 +174,49 @@ class HealthIcon extends FlxSprite implements IUiSprite
 	{
 		if (!updateFrames) return;
 		
-		animation.frameIndex = health < 0.2 ? 1 : 0;
+		if (isAnimated)
+		{
+			switch (getCurrentAnimation())
+			{
+				case 'idle':
+					if (health < 20) playAnimation('toLosing', 'losing');
+					else if (health > 80) playAnimation('toWinning', 'winning');
+					else playAnimation('idle');
+				case 'winning':
+					if (health < 80) playAnimation('fromWinning', 'idle');
+					else playAnimation('winning', 'idle');
+				case 'losing':
+					if (health > 20) playAnimation('fromLosing', 'idle');
+					else playAnimation('losing', 'idle');
+				case 'toLosing':
+					if (isAnimationFinished()) playAnimation('losing', 'idle');
+				case 'toWinning':
+					if (isAnimationFinished()) playAnimation('winning', 'idle');
+				case 'fromLosing' | 'fromWinning':
+					if (isAnimationFinished()) playAnimation('idle');
+				case '':
+					playAnimation('idle');
+				default:
+					playAnimation('idle');
+			}
+		}
+		else animation.frameIndex = health < 0.2 ? 1 : (health > 0.8 && hasWinIcon) ? 2 : 0;
+	}
+	
+	public function playAnimation(name:String, fallback:String = null, restart = false):Void
+	{
+		// Attempt to play the animation
+		if (hasAnimation(name))
+		{
+			animation.play(name, restart, false, 0);
+			return;
+		}
+		
+		// Play the fallback animation if the requested animation was not found
+		if (fallback != null && hasAnimation(fallback))
+		{
+			animation.play(fallback, restart, false, 0);
+			return;
+		}
 	}
 }
