b81a7180c06c917dbd218c548b9287b308f2f521
Ender69 <112335251+ender69flock@users.noreply.github.com>
2026-03-25 15:46:35 +0500

    i need to time so yeah

--- source/funkin/objects/HealthIcon.hx
+++ source/funkin/objects/HealthIcon.hx
@@ -98,7 +98,7 @@ class HealthIcon extends FlxSprite implements IUiSprite
 		if (!Paths.fileExists('images/' + name + '.png')) name = 'icons/icon-face'; // Prevents crash from missing icon
 		
 		final graphic = Paths.image(name, null, false);
-		
+		var iSize:Float = Math.round(graphic.width / graphic.height);
 		loadGraphic(graphic, true, Math.floor(graphic.width / frameCount), Math.floor(graphic.height));
 		iconOffsets[0] = (width - 150) / 2;
 		iconOffsets[1] = (width - 150) / 2;
