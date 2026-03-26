f2183de7625c9ed82a5b7726233554b4194c0882
Ender69 <112335251+ender69flock@users.noreply.github.com>
2026-03-26 12:59:22 +0500

    Adjust evilSnow position and timer durations

--- content/base-game/data/stages/mallEvil/script.hx
+++ content/base-game/data/stages/mallEvil/script.hx
@@ -8,7 +8,7 @@ function onLoad()
 	var evilTree:BGSprite = new BGSprite('christmas/evilTree', 300, -300, 0.2, 0.2);
 	add(evilTree);
 	
-	var evilSnow:BGSprite = new BGSprite('christmas/evilSnow', -200, 700);
+	var evilSnow:BGSprite = new BGSprite('christmas/evilSnow', -250, 700);
 	add(evilSnow);
 }
 
@@ -22,18 +22,16 @@ function onStartCountdown()
 		camHUD.alpha = 0.0001;
 		inCutscene = true;
 		
-		new FlxTimer().start(0.01, function(tmr:FlxTimer) FlxG.camera.fade(FlxColor.BLACK, 0.7, true));
+		new FlxTimer().start(0.05, function(tmr:FlxTimer) FlxG.camera.fade(FlxColor.BLACK, 0.7, true));
 		FlxG.sound.play(Paths.sound('Lights_Turn_On'));
 		snapCamToPos(400, -2050);
 		FlxG.camera.zoom = 1.5;
 		
-		new FlxTimer().start(0.8, function(tmr:FlxTimer)
-        {
+		new FlxTimer().start(0.8, function(tmr:FlxTimer) {
 			FlxTween.tween(FlxG.camera, {zoom: defaultCamZoom}, 2.5,
 				{
 					ease: FlxEase.quadInOut,
-					onComplete: function(twn:FlxTween)
-                    {
+					onComplete: function(twn:FlxTween) {
 						FlxTween.tween(camHUD, {alpha: 1}, 0.7);
 						allowCountdown = true;
 						inCutscene = false;
